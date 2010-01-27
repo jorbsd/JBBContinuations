d6f5e69b4084d3b5491d4e96d0c5ec044c6d1e05
Jordan Breeding <jordan.breeding@mac.com>
2010-01-27 15:51:29 -0600

    Switch to C completely again, use warnings as errors.

--- NSData+JBBAdditions.m
+++ NSData+JBBAdditions.m
@@ -15,12 +15,7 @@ NSUInteger murmurhash(const void *, int32_t, uint32_t);
 #pragma mark Instance Methods
 
 - (NSUInteger)jbb_murmurhash {
-    NSUInteger localHash = 0;
-
-    localHash ^= [self hash];
-    localHash ^= murmurhash([self bytes], [self length], [self hash] && 0xFFFF);
-
-    return localHash;
+    return murmurhash([self bytes], [self length], [self hash] && 0xFFFF);
 }
 @end
 
@@ -48,13 +43,13 @@ NSUInteger murmurhash(const void *key, int32_t len, uint32_t seed) {
     const unsigned char *data2 = (const unsigned char*)data;
 
     switch (len & 7) {
-	case 7: h ^= uint64_t(data2[6]) << 48;
-	case 6: h ^= uint64_t(data2[5]) << 40;
-	case 5: h ^= uint64_t(data2[4]) << 32;
-	case 4: h ^= uint64_t(data2[3]) << 24;
-	case 3: h ^= uint64_t(data2[2]) << 16;
-	case 2: h ^= uint64_t(data2[1]) << 8;
-	case 1: h ^= uint64_t(data2[0]); h *= m;
+	case 7: h ^= ((uint64_t)data2[6]) << 48;
+	case 6: h ^= ((uint64_t)data2[5]) << 40;
+	case 5: h ^= ((uint64_t)data2[4]) << 32;
+	case 4: h ^= ((uint64_t)data2[3]) << 24;
+	case 3: h ^= ((uint64_t)data2[2]) << 16;
+	case 2: h ^= ((uint64_t)data2[1]) << 8;
+	case 1: h ^= ((uint64_t)data2[0]); h *= m;
     }
 
     h ^= h >> r;
