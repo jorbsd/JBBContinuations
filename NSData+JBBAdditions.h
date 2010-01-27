20a47044d8f1e6e5acf96ba50da53c8d20525ec9
Jordan Breeding <jordan.breeding@mac.com>
2010-01-26 21:50:34 -0600

    Add -[NSData jbb_murmurhash].

---
+++ NSData+JBBAdditions.h
@@ -0,0 +1,17 @@
+//
+//  NSData+JBBAdditions.h
+//  JBBAdditions
+//
+//  Created by Jordan Breeding on 2008/10/18.
+//  Copyright 2008 Jordan Breeding. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (JBBAdditions)
+
+#pragma mark Instance Methods
+
+- (NSUInteger)jbb_murmurhash;
+@end
+
