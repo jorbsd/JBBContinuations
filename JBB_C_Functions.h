3c4dab21cc8123c45940b6b905fb00a4e9f51a68
Jordan Breeding <jordan.breeding@mac.com>
2010-03-03 15:47:55 -0600

    Remove comment.

--- JBB_C_Functions.h
+++ JBB_C_Functions.h
@@ -8,9 +8,6 @@
 //  BSD License, Use at your own risk
 //
 
-// jbb_removeObjCTypeQualifiers(const char *aType) returns memory from
-// malloc()
-
 extern const char* jbb_removeObjCTypeQualifiers(const char *aType);
 extern NSString* jbb_NSStringFromCString(const char *aString);
 
